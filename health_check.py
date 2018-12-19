e42c4eca8531d394ac3ce932e84ca4ae0cf87cc2
zhangchenggong <chenggong.zhang@igen-tech.com>
2018-12-19 19:51:28 +0800

    modify health_check.py

--- health_check.py
+++ health_check.py
@@ -4,6 +4,7 @@ __auth__ = 'chicol'
 
 import json
 import os,re
+import itertools
 from common.common import transfer_seperator
 
 
@@ -13,33 +14,35 @@ def is_data_exists(data, condition):
     return res
 
 
-def judge_condition(data, conditions):
+def judge_condition(data, conditions, results):
 
     key = list(conditions.keys())[0]
     value = list(conditions.values())[0]
     if len(value) == 5:
         print("conditions: ", is_data_exists(data, value))
         if is_data_exists(data, value):
-            data_results.append(key)
+            results.append(key)
         return is_data_exists(data, value)
     elif len(value) > 5:
         print("conditions: {}".format(value))
-        condition_list = re.split('(F[A|B|C][0-9]{2}[F|T|S|E])', value)
+        condition_list = re.split('(F[A|B|C][0-9]{2}[F\|T|S|E])', value)
         while '' in condition_list:
             condition_list.remove('')
-        # 取list中第奇数个元素 ==> 条件
-        condition_key_list = condition_list[0::2]
-        condition_operator_list = condition_list[1::2]
+        # 取list中第奇数个元素 ==> 条件;取list中第偶数个元素 ==> 操作符
+        condition_list1 = condition_list[0::2]
+        condition_list2 = condition_list[1::2]
+        if len(condition_list1) != len(condition_list2):
+            if len(condition_list1) > len(condition_list2):
+                condition_list2.append('')
+            else:
+                condition_list1.append('')
         print("condition_list: {}".format(condition_list))
-        print("condition_key_list: {}".format(condition_key_list))
-        print("condition_operator_list: {}".format(condition_operator_list))
         conditions_str = ''
-        for key,operator in zip(condition_key_list,condition_operator_list):
-            print("key: {}, operator: {}".format(key, operator))
+        for key, operator in itertools.zip_longest(condition_list1, condition_list2):
             conditions_str += str(is_data_exists(data, key)) + operator
         print("conditions_str: {}".format(conditions_str))
-
-        # conditions = 'FA02F&FA03F'
+        if eval(conditions_str):
+            results.append(key)
 
 
 if '__main__' == __name__:
@@ -52,14 +55,23 @@ if '__main__' == __name__:
         print(rules)
     dataQ = rules['dataQuality']
     data_no_pass = dataQ['noPass']
+    data_pass = dataQ['pass']
     data_results = []
     station_results = []
     security_results = []
     station_level = None
     print(data_no_pass)
-    condition = data_no_pass['conditions'][1]
-    print(condition)
+    # conditions = data_no_pass['conditions']
+    # print(conditions)
     # print("value: {}".format(condition.values()))
     condition1 = {'FAR07F': 'FA01T&(FA02T|(FA03T&FA18T&FA19T))&FA04T&FA05T&(FA06T|FA07T)&(FA08T|FA10T)&(FA11T|FA12T|FA13T|FA14T)&FA15T&FA16T&FA17T'}
     # judge_condition(data, list(condition.values())[0])
-    judge_condition(data, condition1)
+    # 检查数据质量--不通过的结果
+    for condition in data_no_pass['conditions']:
+        judge_condition(data, condition,data_results)
+        print("data_results: {}".format(data_results))
+    # 检查数据质量--通过的结果
+    for condition in data_pass['conditions']:
+        judge_condition(data, condition, data_results)
+        print("data_results: {}".format(data_results))
+
